7b51ae43562f1015f2155d61d0dde5078e722b8e
David <davidyuen0305@gmail.com>
2020-10-20 19:12:23 -0700

    deployed on firebase.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Resume from "./images/Resume.pdf";
+import "./Resume.css";
 
 const ResumePdf = () => {
   return (
     <div>
-      <iframe src={Resume} title="title" width="70%" height="800px">
+      <iframe src={Resume} title="title">
         Press me: <a href={Resume}>Download PDF</a>
       </iframe>
     </div>
